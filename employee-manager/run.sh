1903c19875584224f67568b6d2432c3dce15a452
k4y0W <wojenskipawel945@gmail.com>
2025-12-17 16:18:43 +0100

    1.0.2

--- employee-manager/run.sh
+++ employee-manager/run.sh
@@ -1,11 +1,4 @@
-#!/bin/sh
+#!/bin/with-contenv bashio
 set -e
-
 echo "Starting Employee Manager Add-on..."
-
-# Opcjonalnie: Tutaj możesz dodać kod, który podmienia token w flows.json 
-# jeśli używasz węzłów konfiguracyjnych, ale Ingress zazwyczaj to upraszcza.
-
-# Uruchomienie Node-RED wskazując nasz plik ustawień
-# --max-old-space-size ogranicza RAM, żeby dodatek nie zjadł całego RPi
 exec node --max-old-space-size=256 /usr/local/bin/node-red -s /data/settings.js
